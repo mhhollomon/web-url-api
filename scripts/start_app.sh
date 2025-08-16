5d74963f1449d797da21c0a1b422cec49dfa6fa0
Mark Hollomon <mhhollomon@gmail.com>
2025-08-16 08:05:44 -0400

    Attempt to be a bit more secure.

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -8,6 +8,6 @@ venv=${code_home}/../.venv
 
 . ${venv}/bin/activate
 
-# siwtch groups before running so the
+# switch groups before running so the
 # unix socket has the correct group.
-sg rproxy python ${code_home}/../app.py
+sg rproxy "python ${code_home}/../app.py"
